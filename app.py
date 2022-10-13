b6b5a762cf79f104450472227f9200827eaf1459
BoogaLechuga <burnermark065@gmail.com>
2022-10-13 17:15:39 -0500

    Message

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def cats() :
-    return{"message":"cats!"}
+
+    url = "https://api.thecatapi.com/v1/images/search?breed_ids=bamb"
+
+    response = requests.get(url)
+
+    json = response.json()
+
+    url = json[0]["url"]
+
+    return '<img src="' + url + '"> </img>'
+
